3a3085ddad1f856ea06bf00606adf44722cbb25a
fabiareor <fabiareor@gmail.com>
2025-02-27 10:19:35 -0500

    Debugging deployed calls

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 
     (process.env.NODE_ENV === 'production' 
-      ? 'http://folioai.tech/'
+      ? 'https://folioai.tech/'
       : 'http://localhost:8000'),
   // Add other configuration variables here
 };
