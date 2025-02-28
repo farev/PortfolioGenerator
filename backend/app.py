119fbc53feb2f8c327b4f6dcb9113a28519a13a8
fabiareor <fabiareor@gmail.com>
2025-02-28 12:35:47 -0500

    Resolving CORS policy error

--- backend/app.py
+++ backend/app.py
@@ -47,12 +47,10 @@ if ENVIRONMENT == 'production':
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
-    max_age=600,
 )
 
 # Add CORS headers based on environment
