d1439b0f4411f68ce3fd658a07d0bcf9c45b994b
fabiareor <fabiareor@gmail.com>
2025-03-02 18:34:14 -0500

    Resolving CORS policy error, added proper backend startup

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /home/site/wwwroot
+# Install dependencies if not already installed
+pip install -r requirements.txt
+# Start the application
 gunicorn --bind=0.0.0.0:$PORT --timeout 600 app:app 
