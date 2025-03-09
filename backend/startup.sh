a626119f6f69e2d2bec3afb43feac7aa289b58c8
fabiareor <fabiareor@gmail.com>
2025-03-08 22:35:57 -0500

    Still fixing backend deployment

--- backend/startup.sh
+++ backend/startup.sh
@@ -5,13 +5,6 @@ log_with_timestamp() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
 
-# Check Python version
-PYTHON_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
-if [[ $(echo "$PYTHON_VERSION >= 3.12" | bc -l) -eq 1 ]]; then
-    log_with_timestamp "Python version $PYTHON_VERSION is not supported. Please use Python 3.11 or lower"
-    exit 1
-fi
-
 # Create virtual environment if it doesn't exist
 if [ ! -d "/home/site/wwwroot/antenv" ]; then
     log_with_timestamp "Creating virtual environment..."
